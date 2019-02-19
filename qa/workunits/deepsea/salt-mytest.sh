fbc2fc638480dc226ab65172781cc496f55eb235
Roman <rgrigorev@suse.de>
2019-02-19 17:14:18 +0100

    add workunit for test

---
+++ qa/workunits/deepsea/salt-mytest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex 
+echo "This is my workunit. It will run on the Salt Master."
+systemctl status salt-master.service
+sudo salt '*' test.ping
+echo "All good"
+echo "qqqqqqqqqqqq"
+
+
